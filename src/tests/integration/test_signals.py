8c3514b91dcde30bed9c3c5bf6abb268ad020582
Tobias Kunze <rixx@cutebit.de>
2017-08-12 17:14:52 +0200

    Test that multiple responses fail

--- src/tests/integration/test_signals.py
+++ src/tests/integration/test_signals.py
@@ -41,3 +41,31 @@ def test_match_no_fee(member, real_transaction):
 
     account = member.get_account(category=AccountCategory.MEMBER_FEES)
     assert account.total() == 0
+
+
+@pytest.mark.django_db
+def test_match_multiple_fees(member, real_transaction):
+
+    @receiver(derive_virtual_transactions, dispatch_uid='test-plugin')
+    def derive_test_transaction(sender, signal):
+        member = Member.objects.first()
+        account = member.get_account(category=AccountCategory.MEMBER_FEES)
+        fee = VirtualTransaction.objects.create(
+            real_transaction=sender,
+            destination_account=account,
+            amount=sender.amount / 2,
+            value_datetime=sender.value_datetime,
+        )
+        account = member.get_account(category=AccountCategory.MEMBER_DONATION)
+        donation = VirtualTransaction.objects.create(
+            real_transaction=sender,
+            destination_account=account,
+            amount=sender.amount / 2,
+            value_datetime=sender.value_datetime,
+        )
+        return [fee, donation]
+
+    real_transaction.derive_virtual_transactions()
+    fees = member.get_account(category=AccountCategory.MEMBER_FEES)
+    donations = member.get_account(category=AccountCategory.MEMBER_DONATION)
+    assert fees.total() + donations.total() == real_transaction.amount
