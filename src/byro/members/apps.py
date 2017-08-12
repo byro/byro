c73a248e1b9f3e5f8ff3936e789b8bcae8ab5d04
Tobias Kunze <rixx@cutebit.de>
2017-08-12 14:28:26 +0200

    [members] Start app with minimal model

---
+++ src/byro/members/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MemberConfig(AppConfig):
+    name = 'byro.members'
+    label = 'byromembers'
---
+++ src/byro/members/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+from byro.common.models.auditable import Auditable
+
+
+class Member(Auditable, models.Model):
+
+    # TODO: flesh out
+    email = models.EmailField()
