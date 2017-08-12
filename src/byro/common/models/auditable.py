c0daece185173ef835a440890cbd7ae3357bc6a9
Tobias Kunze <rixx@cutebit.de>
2017-08-12 14:27:09 +0200

    Add common app
    Contains the Auditable class for timestamping (datetime + optional user)
    of models, and the Choices class for CharField choices.

---
+++ src/byro/common/models/auditable.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+class Auditable:
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    created_by = models.ForeignKey(
+        to=get_user_model(),
+        on_delete=models.PROTECT,
+        related_name='+',  # no related lookup
+        null=True
+    )
+    modified_by = models.ForeignKey(
+        to=get_user_model(),
+        on_delete=models.PROTECT,
+        related_name='+',  # no related lookup
+        null=True
+    )
