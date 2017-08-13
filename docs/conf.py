4c7b6a4a199315f4cbee0f4abca2f475d16448cd
Tobias Kunze <rixx@cutebit.de>
2017-08-13 19:32:36 +0200

    [docs] Use sphinx theme locally

--- docs/conf.py
+++ docs/conf.py
@@ -65,6 +65,7 @@ todo_include_todos = False
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
 
 
 # -- Options for HTMLHelp output ------------------------------------------
