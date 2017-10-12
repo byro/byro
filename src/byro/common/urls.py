46e7e21eb1a5f53e193f0d700af3eb945fa50311
luto <m@luto.at>
2017-10-12 22:57:49 +0200

    fix pylama for common

--- src/byro/common/urls.py
+++ src/byro/common/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 
 from .views import LoginView, logout_view
 
--- src/byro/common/views.py
+++ src/byro/common/views.py
@@ -1,11 +1,9 @@
-import random
 import urllib
 
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import redirect
-from django.urls import reverse
 from django.utils.http import is_safe_url
 from django.utils.translation import ugettext as _
 from django.views.generic import TemplateView
