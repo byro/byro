70d545532ca31b72cd46d20e692427bd35b4dd21
Tobias Kunze <rixx@cutebit.de>
2017-10-12 20:57:15 +0200

    [members] Add basic fields to member model

--- src/byro/members/models.py
+++ src/byro/members/models.py
@@ -5,8 +5,22 @@ from byro.common.models.auditable import Auditable
 
 class Member(Auditable, models.Model):
 
-    # TODO: flesh out
-    email = models.EmailField()
+    number = models.CharField(
+        max_length=100,
+        null=True, blank=True,
+    )
+    name = models.CharField(
+        max_length=100,
+        null=True, blank=True,
+    )
+    address = models.CharField(
+        max_length=300,
+        null=True, blank=True,
+    )
+    email = models.EmailField(
+        max_length=200,
+        null=True, blank=True,
+    )
 
     @property
     def profiles(self) -> list:
