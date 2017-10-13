2ed5870e6bf3d6d8356ca4ddb0b2145ff40403ec
Andreas Madsack <andreas@madflex.de>
2017-10-13 11:53:52 +0200

    add first test for profile plugin

---
+++ src/byro/plugins/profile/tests/test_profile_model.py
@@ -0,0 +1,16 @@
+import pytest
+
+from byro.members.models import Member
+
+
+@pytest.fixture
+def member_bob():
+    member = Member.objects.create(email='bob@hacker.space')
+    yield member
+    [profile.delete() for profile in member.profiles]
+    member.delete()
+
+
+@pytest.mark.django_db
+def test_profile_models(member_bob):
+    assert member_bob.profile_profile.nick is None
