0a01370d0dca4af349e8152a4519e06f459354b8
Tobias Kunze <rixx@cutebit.de>
2017-10-13 16:35:10 +0200

    [office] Add and style a dashboard

--- src/byro/office/views/dashboard.py
+++ src/byro/office/views/dashboard.py
@@ -1,5 +1,13 @@
 from django.views.generic import TemplateView
 
+from byro.members.models import Member, Membership
+
 
 class DashboardView(TemplateView):
     template_name = 'office/dashboard.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['member_count'] = Member.objects.all().count()
+        context['active_count'] = Membership.objects.filter(end__isnull=True).count()
+        return context
