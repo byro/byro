6e107afb5db3396edb0fde6b9932c2b91504bb50
Andreas Madsack <andreas@madflex.de>
2017-10-13 11:53:59 +0200

    use different fixture name than in main tests

--- src/byro/plugins/sepa/tests/test_sepa_model.py
+++ src/byro/plugins/sepa/tests/test_sepa_model.py
@@ -4,13 +4,13 @@ from byro.members.models import Member
 
 
 @pytest.fixture
-def member():
-    member = Member.objects.create(email='joe@hacker.space')
+def member_bob():
+    member = Member.objects.create(email='bob@hacker.space')
     yield member
     [profile.delete() for profile in member.profiles]
     member.delete()
 
 
 @pytest.mark.django_db
-def test_sepa_models(member):
-    assert member.profile_sepa.iban is None
+def test_sepa_models(member_bob):
+    assert member_bob.profile_sepa.iban is None
