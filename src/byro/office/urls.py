58422df16025104f9ca0d638d44ab3cc5db82253
Tobias Kunze <rixx@cutebit.de>
2017-10-13 16:58:07 +0200

    [office] Add settings submenu

--- src/byro/office/urls.py
+++ src/byro/office/urls.py
@@ -7,7 +7,7 @@ from .views import (
 
 office_urls = [
     url('^settings/registration$', RegistrationConfigView.as_view(), name='settings.registration'),
-    url('^settings$', ConfigurationView.as_view(), name='settings'),
+    url('^settings$', ConfigurationView.as_view(), name='settings.base'),
     url('^$', DashboardView.as_view(), name='dashboard'),
     url(r'^members/list', MemberListView.as_view(), name='members.list'),
     url(r'^members/add', MemberCreateView.as_view(), name='members.add'),
