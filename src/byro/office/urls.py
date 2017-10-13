5aa4f79915795f250f8e59fac0d115a26301401e
luto <m@luto.at>
2017-10-13 12:01:56 +0200

    add very basic member list and detail views

--- src/byro/office/urls.py
+++ src/byro/office/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
-from .views import DashboardView
+from .views import DashboardView, MemberListView, MemberDetailView
 
 office_urls = [
     url('^$', DashboardView.as_view(), name='dashboard'),
+    url(r'^members/list', MemberListView.as_view(), name='members.list'),
+    url(r'^members/view/(?P<pk>\d+)', MemberDetailView.as_view(), name='members.detail'),
 ]
--- src/byro/office/views.py
+++ src/byro/office/views.py
@@ -1,5 +1,19 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+
+from byro.members.models import Member
 
 
 class DashboardView(TemplateView):
     template_name = 'office/dashboard.html'
+
+
+class MemberListView(ListView):
+    template_name = 'office/member_list.html'
+    context_object_name = 'members'
+    model = Member
+
+
+class MemberDetailView(DetailView):
+    template_name = 'office/member_detail.html'
+    context_object_name = 'member'
+    model = Member
