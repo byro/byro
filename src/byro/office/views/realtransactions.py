0881ec3f6dac5243ce279f905ab1430337bc508e
Tobias Kunze <rixx@cutebit.de>
2017-10-14 14:01:35 +0200

    [office] Fix formset generation

--- src/byro/office/views/realtransactions.py
+++ src/byro/office/views/realtransactions.py
@@ -1,12 +1,6 @@
-from django import forms
-from django.contrib import messages
-from django.db.models import Q
-from django.forms.models import inlineformset_factory
-from django.shortcuts import redirect
-from django.urls import reverse
-from django.utils.timezone import now
-from django.utils.translation import ugettext_lazy as _
-from django.views.generic import DetailView, FormView, ListView
+from django.forms.models import BaseModelFormSet, inlineformset_factory
+from django.utils.functional import cached_property
+from django.views.generic import ListView
 
 from byro.bookkeeping.forms import VirtualTransactionForm
 from byro.bookkeeping.models import RealTransaction, VirtualTransaction
@@ -15,23 +9,28 @@ from byro.bookkeeping.models import RealTransaction, VirtualTransaction
 class RealTransactionListView(ListView):
     template_name = 'office/realtransaction/list.html'
     context_object_name = 'transactions'
+    paginate_by = 25
     model = RealTransaction
 
-    @property
+    def get_queryset(self):
+        return super().get_queryset().filter(virtual_transactions__isnull=False)
+
+    @cached_property
     def formset_class(self):
         formset_class = inlineformset_factory(
             RealTransaction, VirtualTransaction, form=VirtualTransactionForm,
-            can_delete=True, extra=0,
+            formset=BaseModelFormSet, can_delete=True, extra=0,
         )
         return formset_class
 
+    def get_formset(self, transaction):
+        return self.formset_class(
+            self.request.POST if self.request.method == 'POST' else None,
+            queryset=VirtualTransaction.objects.filter(real_transaction=transaction)
+        )
+
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
-
-        for rtrans in ctx['transactions']:
-            rtrans.vt_formset = self.formset_class(
-                self.request.POST if self.request.method == 'POST' else None,
-                queryset=VirtualTransaction.objects.filter(real_transaction=rtrans)
-            )
-
+        for transaction in ctx['transactions']:
+            transaction.vt_formset = self.get_formset(transaction)
         return ctx
