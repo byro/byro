afa507e03c3e7daa03034db2ddb8a15d9d62b960
Andreas Madsack <andreas@madflex.de>
2017-10-14 17:02:09 +0200

    added member_name as hidden field in formset
    is not used in javascript part yet.

--- src/byro/bookkeeping/forms.py
+++ src/byro/bookkeeping/forms.py
@@ -5,11 +5,12 @@ from .models import VirtualTransaction
 
 class VirtualTransactionForm(forms.ModelForm):
     member = forms.CharField(widget=forms.TextInput(attrs={'class': 'member-typeahead'}))
+    member_name = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     def __init__(self, *args, **kwargs):
         initial = kwargs.get('initial', {})
         if kwargs.get('instance'):
-            initial.update({'member': kwargs.get('instance').member.name})
+            initial.update({'member_name': kwargs.get('instance').member.name})
         kwargs['initial'] = initial
         super().__init__(*args, **kwargs)
 
