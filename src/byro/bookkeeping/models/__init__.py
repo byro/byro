9511120ece96232f2ca0d596daea23c048d11991
Tobias Kunze <rixx@cutebit.de>
2018-01-12 23:32:14 +0100

    Add missing import

--- src/byro/bookkeeping/models/__init__.py
+++ src/byro/bookkeeping/models/__init__.py
@@ -1,11 +1,14 @@
 from .account import Account, AccountCategory
-from .real_transaction import RealTransaction, TransactionChannel
+from .real_transaction import (
+    RealTransaction, RealTransactionSource, TransactionChannel,
+)
 from .virtual_transaction import VirtualTransaction
 
 __all__ = (
     'Account',
     'AccountCategory',
     'RealTransaction',
+    'RealTransactionSource',
     'TransactionChannel',
     'VirtualTransaction'
 )
