70b5db0d06d9f4bd4f12aa448f0c67dd1b37b7e0
Tobias Kunze <rixx@cutebit.de>
2018-01-13 14:12:40 +0100

    [bookkeeping] Track upload file state

--- src/byro/bookkeeping/models/real_transaction.py
+++ src/byro/bookkeeping/models/real_transaction.py
@@ -5,8 +5,18 @@ from byro.common.models.auditable import Auditable
 from byro.common.models.choices import Choices
 
 
+class SourceState(Choices):
+    NEW = 'new'
+    PROCESSING = 'processing'
+    PROCESSED = 'processed'
+    FAILED = 'failed'
+
+    valid_choices = [NEW, PROCESSING, PROCESSED, FAILED]
+
+
 class RealTransactionSource(Auditable, models.Model):
     source_file = models.FileField(upload_to='transaction_uploads/')
+    state = models.CharField(default=SourceState.NEW, choices=SourceState.choices, max_length=SourceState.max_length)
 
     @transaction.atomic
     def process(self):
