52d4832cade022b71a9a38ab585cf07d775d8781
Tobias Kunze <rixx@cutebit.de>
2018-01-24 16:24:45 +0100

    Let plugins provide URLs

--- src/byro/urls.py
+++ src/byro/urls.py
@@ -13,11 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+import importlib.util
+
+from django.apps import apps
 from django.conf.urls import include, url
 from django.contrib import admin
+
+raw_plugin_patterns = []
+for app in apps.get_app_configs():
+    if hasattr(app, 'ByroPluginMeta'):
+        if importlib.util.find_spec(app.name + '.urls'):
+            urlmod = importlib.import_module(app.name + '.urls')
+            single_plugin_patterns = []
+            if hasattr(urlmod, 'urlpatterns'):
+                single_plugin_patterns += urlmod.urlpatterns
+            raw_plugin_patterns.append(
+                url(r'', include((single_plugin_patterns, app.label)))
+            )
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('byro.common.urls', namespace='common')),
     url(r'', include('byro.office.urls', namespace='office')),
+    url(r'', include((raw_plugin_patterns, 'plugins')))
 ]
