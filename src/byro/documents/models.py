d1eccb04bebcc203139b3859a9b1507418e36fb2
Tobias Kunze <rixx@cutebit.de>
2018-01-24 16:49:29 +0100

    [documents] Helper method to send a document

--- src/byro/documents/models.py
+++ src/byro/documents/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from byro.common.models.auditable import Auditable
 from byro.common.models.choices import Choices
@@ -25,3 +26,27 @@ class Document(Auditable, models.Model):
         on_delete=models.SET_NULL,
         null=True, blank=True,
     )
+
+    template = _('''
+Hi, {name},
+
+Please find attached a document we wanted to send you/that you requested.
+
+Thank you,
+{association}
+''').strip()
+
+    def send(self, immediately=False, text=None, subject=None, email=None):
+        from byro.common.models import Configuration
+        from byro.mails.models import EMail
+        us = Configuration.get_solo().name
+        mail = EMail.objects.create(
+            to=email or self.member.email,
+            text=text or self.template.format(name=self.member.name if self.member else email, association=us),
+            subject=_('[{association}] Your document').format(association=us)
+        )
+        mail.attachments.add(self)
+        mail.save()
+        if immediately:
+            mail.send()
+        return mail
