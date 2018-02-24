43f254a6f247208a6232ee06b183ed00f0169492
Tobias Kunze <rixx@cutebit.de>
2018-02-24 23:51:41 +0100

    [office] Add mail detail view

--- src/byro/office/views/mails.py
+++ src/byro/office/views/mails.py
@@ -1,12 +1,23 @@
-from django.views.generic import DetailView, ListView, View
+from django import forms
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
+from django.views.generic import DetailView, ListView, UpdateView, View
 
 from byro.mails.models import EMail, MailTemplate
 
 
-class MailDetail(DetailView):  # TODO
+class MailDetail(UpdateView):
     queryset = EMail.objects.filter(sent__isnull=True)
     template_name = 'office/mails/detail.html'
     context_object_name = 'mail'
+    form_class = forms.modelform_factory(EMail, fields=['to', 'reply_to', 'cc', 'bcc', 'subject', 'text'])
+    success_url = '/mails/outbox'
+
+    def form_valid(self, form):
+        if form.instance.sent:
+            raise forms.ValidationError(_('This mail has been sent already, and cannot be modified. Copy it to a draft instead!'))
+        messages.success(self.request, _('Your changes have been saved.'))
+        return super().form_valid(form)
 
 
 class MailCopy(View):  # TODO
