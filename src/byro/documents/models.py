a6eed5e4e48388ca7c007d81fe05bc9422a4e954
Tobias Kunze <rixx@cutebit.de>
2018-02-26 17:46:49 +0100

    [documents] Add optional title to documents

--- src/byro/documents/models.py
+++ src/byro/documents/models.py
@@ -14,6 +14,7 @@ class DocumentDirection(Choices):
 
 class Document(Auditable, models.Model):
     document = models.FileField(upload_to='documents/')
+    title = models.CharField(max_length=300, null=True)
     category = models.CharField(max_length=300, null=True)
     direction = models.CharField(
         choices=DocumentDirection.choices,
