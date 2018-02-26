f39abbbe2cea3963fc6a1be84a432ca7d42730b0
Tobias Kunze <rixx@cutebit.de>
2018-02-26 16:44:55 +0100

    [office] Add member view signal

--- src/byro/office/apps.py
+++ src/byro/office/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class OfficeConfig(AppConfig):
     name = 'byro.office'
+
+    def ready(self):
+        from . import signals  # noqa
---
+++ src/byro/office/signals.py
@@ -0,0 +1,13 @@
+import django.dispatch
+
+member_view = django.dispatch.Signal()
+"""
+receives the member as sender.
+returns a dict:
+
+    {
+        "label": _("Fancy Member View"),
+        "url": "/member/123/foo/",
+        "url_name": "plugins:myplugin:foo_view",
+    }
+"""
