4c8b492dc2000f4fd1be1046157dae311ba78675
Tobias Kunze <rixx@cutebit.de>
2018-05-11 13:07:48 +0200

    Document new signals

--- src/byro/members/signals.py
+++ src/byro/members/signals.py
@@ -17,12 +17,12 @@ message will be displayed in the frontend as a warning.
 """
 leave_member_mail_information = django.dispatch.Signal()
 """
-Receives the leave of a member as signal. Response will be added to the email
+Receives the leaving member as signal. Response will be added to the email
 confirming termination to the member.
 """
 leave_member_office_mail_information = django.dispatch.Signal()
 """
-Receives the leave of a member as signal. Response will be added to the email
+Receives the leaving member as signal. Response will be added to the email
 notifying the office about the termination of the member.
 """
 leave_member = django.dispatch.Signal()
