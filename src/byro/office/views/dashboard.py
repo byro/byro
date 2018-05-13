fef7e89c14706e32f124cedc843888fc5384815b
Henryk Plötz <henryk@ploetzli.ch>
2018-05-13 22:26:43 +0200

    [office] Also fix active member count in Dashboard

--- src/byro/office/views/dashboard.py
+++ src/byro/office/views/dashboard.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+from django.utils.timezone import now
 from django.views.generic import TemplateView
 
 from byro.bookkeeping.models import RealTransaction
@@ -11,7 +13,7 @@ class DashboardView(TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         context['member_count'] = Member.objects.all().count()
-        context['active_count'] = Membership.objects.filter(end__isnull=True).count()
+        context['active_count'] = Membership.objects.filter(Q(start__lte=now().date()) & (Q(end__isnull=True) | Q(end__gte=now().date()))).count()
         context['unmapped_transactions_count'] = RealTransaction.objects.filter(virtual_transactions__isnull=True).count()
         context['stats'] = get_member_statistics()
         return context
