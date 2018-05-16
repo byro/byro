498d3e5cf746550cf367f65e4c451aaff0cebbdd
Henryk Plötz <henryk@ploetzli.ch>
2018-05-16 17:36:09 +0200

    Much more default types

--- src/byro/common/forms/registration.py
+++ src/byro/common/forms/registration.py
@@ -1,65 +1,126 @@
 from collections import OrderedDict
 
 from django import forms
+from django.db import models
+from django.utils.decorators import classproperty
 from django.utils.translation import ugettext_lazy as _
 
 from byro.common.models import Configuration
-from byro.members.models import Member, FeeIntervals
+from byro.members.models import Member, Membership, FeeIntervals
+
+class DefaultDates:
+    TODAY = 'today'
+    BEGINNING_MONTH = 'beginning_month'
+    BEGINNING_YEAR = 'beginning_year'
+
+    @classproperty
+    def choices(cls):
+        return (
+            (None, '------------'),
+            (cls.TODAY, _('Current day')),
+            (cls.BEGINNING_MONTH, _('Beginning of current month')),
+            (cls.BEGINNING_YEAR, _('Beginning of current year')),
+        )
+
+class DefaultBoolean:
+    @classproperty
+    def choices(cls):
+        return (
+            (None, '------------'),
+            (False, _('False')),
+            (True, _('True')),
+        )
+
+SPECIAL_NAMES = {
+    Member: 'member',
+    Membership: 'membership',
+}
 
 class RegistrationConfigForm(forms.Form):
-    member__number_position = forms.IntegerField(required=False, label=_('Membership number/ID'))
-    member__name_position = forms.IntegerField(required=False, label=_('Name'))
-    member__address_position = forms.IntegerField(required=False, label=_('Address'))
-    member__email_position = forms.IntegerField(required=False, label=_('E-Mail'))
-    member__member_contact_type_position = forms.IntegerField(required=False, label=_('Contact type'))
-    membership__start_position = forms.IntegerField(required=False, label=_('Join date'))
-    membership__start_default = forms.ChoiceField(required=False, label=_('Default join date'),
-        choices=((None,'------------'),('today',_('Current day')),('beginning_mongth',_('Beginning of current month')),('beginning_year',_('Beginning of current year'))))
-    membership__amount_position = forms.IntegerField(required=False, label=_('Membership fee'))
-    membership__amount_default = forms.IntegerField(required=False, label=_('Default membership fee'))
-    membership__interval_position = forms.IntegerField(required=False, label=_('Payment interval'))
-    membership__interval_default = forms.ChoiceField(required=False, label=_('Default payment interval'), 
-        choices=((None,'------------'),)+FeeIntervals.choices)
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for profile in Member.profile_classes:
-            for field in profile._meta.fields:
-                if field.name not in ('id', 'member'):
-                    key = '{profile.__name__}__{field.name}_position'.format(profile=profile, field=field)
-                    label = '{field_name} ({profile.__name__})'.format(field_name=field.verbose_name or field.name, profile=profile)
-                    self.fields[key] = forms.IntegerField(required=False, label=label)
+        self.fields_extra = OrderedDict()
+        field_data = []
         config = Configuration.get_solo().registration_form or []
         data = {}
-        field_sort_order = {}
         for entry in config:
             if 'name' not in entry:
                 continue
             data[entry['name']] = entry
-        for full_name, field in self.fields.items():
-            name, key = full_name.rsplit("_",1)
-            entry = data.get(name, {})
-            sort_order = entry.get('position', None) or 998
-            # Order by:
-            #  + Position in form, if set (or 998 as default)
-            #  + 'member' fields first
-            #  + Name of base field
-            #  + 'position' field first
-            #  + Name of key
-            field_sort_order[field] = (sort_order, 0 if name.startswith("member") else 1, name, key != 'position', key)
-            if key in entry:
-                field.initial = entry[key]
-            if key == 'position':
-                field.widget.attrs['placeholder'] = _('Position in form')
-            else:
-                field.widget.attrs['placeholder'] = '' # FIXME
+        for model in [Member, Membership] + Member.profile_classes:
+            for field in model._meta.fields:
+                if field.name in ('id', 'member'):
+                    continue
+                if model is Member and field.name in ('membership_type', ):
+                    continue
 
-        print(field_sort_order)
-        self.fields = OrderedDict(sorted(self.fields.items(), key=lambda x: field_sort_order.get(x[1], None) or 999))
+                key = '{}__{}'.format(SPECIAL_NAMES.get(model, model.__name__), field.name)
+                entry = data.get(key, {})
+
+                verbose_name = field.verbose_name or field.name
+                if model not in SPECIAL_NAMES:
+                    verbose_name = '{verbose_name} ({model.__name__})'.format(verbose_name=verbose_name, model=model)
+                form_fields = OrderedDict()
+
+                position_field = forms.IntegerField(required=False, label=_("Position in form"))
+                form_fields['{}__position'.format(key)] = position_field
+
+                if isinstance(field, models.DateField):
+                    form_fields['{}__default_date'.format(key)] = forms.ChoiceField(required=False, label=_('Default date'), choices=DefaultDates.choices)
+
+                default_field = None
+                choices = getattr(field, 'choices', None)
+                if choices:
+                    default_field = forms.ChoiceField(
+                        required=False, 
+                        label=_('Default value'),
+                        choices=[(None, '-----------')]+list(choices),
+                        initial=field.default)
+                elif not(model is Member and field.name == 'number'):
+                    if isinstance(field, models.CharField):
+                        default_field = forms.CharField(required=False,
+                            label=_('Default value'),
+                            initial=field.default)
+                    elif isinstance(field, models.DecimalField):
+                        default_field = forms.DecimalField(required=False, 
+                            label=_('Default value'), 
+                            max_digits=field.max_digits,
+                            decimal_places=field.decimal_places,
+                            initial=field.default)
+                    elif isinstance(field, models.BooleanField):
+                        default_field = forms.ChoiceField(required=False,
+                            label=_('Default value'),
+                            choices=DefaultBoolean.choices,
+                            initial=field.default)
+
+                if default_field:
+                    form_fields['{}__default'.format(key)] = default_field
+
+                for full_name, form_field in form_fields.items():
+                    value_name = full_name.rsplit('__',1)[-1]
+                    form_field.initial = entry.get(value_name, form_field.initial) 
+
+                field_data.append((
+                    data.get(key, {}).get('position', None) or 998,
+                    0 if model in SPECIAL_NAMES else 1,
+                    key,
+                    verbose_name,
+                    form_fields
+                ))
+
+        # Sort model fields, by:
+        #  + Position in form, if set (use 998 as default)
+        #  + SPECIAL_NAMES first
+        #  + key ({model.name}__{field.name})
+        field_data.sort()
+
+        for _ignore, _ignore, key, verbose_name, form_fields in field_data:
+            self.fields_extra[key] = (verbose_name, (self[name] for name in form_fields.keys()))
+            self.fields.update(form_fields)
 
     def clean(self):
         ret = super().clean()
-        positions = [value for (key,value) in ret.items() if key.endswith("_position") and value is not None]
+        positions = [value for (key,value) in ret.items() if key.endswith("__position") and value is not None]
         if not len(list(positions)) == len(set(positions)):
             raise forms.ValidationError('Every position must be unique!')
         return ret
@@ -67,7 +128,7 @@ class RegistrationConfigForm(forms.Form):
     def save(self):
         data = {}
         for full_name, value in self.cleaned_data.items():
-            name, key = full_name.rsplit("_",1)
+            name, key = full_name.rsplit("__",1)
             data.setdefault(name, {})[key] = value
         data = [dict(name=key, **value) for (key, value) in data.items()]
         config = Configuration.get_solo()
