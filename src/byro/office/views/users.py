a5442d0965ad59040cf877e0349f3806b07830ed
Tobias Kunze <rixx@cutebit.de>
2018-06-07 22:47:14 +0200

    [office] Fix user creation/editing

--- src/byro/office/views/users.py
+++ src/byro/office/views/users.py
@@ -1,7 +1,30 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
 from django.views.generic import DetailView, FormView, ListView, UpdateView
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(label=_('Password'), widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['last_name'].label = _('Name')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        password = self.cleaned_data.get('password')
+        if password:
+            self.instance.set_password(password)
+            self.instance.save()
+
+    class Meta:
+        model = User
+        fields = [
+            'username', 'last_name', 'email', 'is_superuser',
+            'is_staff', 'is_staff',
+        ]
 
 
 class UserListView(ListView):
@@ -14,7 +37,7 @@ class UserListView(ListView):
 class UserCreateView(FormView):
     template_name = 'office/user/add.html'
     model = User
-    form_class = forms.modelform_factory(User, fields=['username', 'password', 'last_name', 'email', 'is_superuser', 'is_staff', 'is_active'])
+    form_class = UserForm
 
     def form_valid(self, form):
         form.save()
@@ -29,13 +52,13 @@ class UserDetailView(UpdateView):
     template_name = 'office/user/detail.html'
     context_object_name = 'user'
     model = User
-    form_class = forms.modelform_factory(User, fields=['last_name', 'email', 'is_superuser', 'is_staff', 'is_active'])
+    form_class = UserForm
 
     def get_object(self):
         return User.objects.get(pk=self.kwargs['pk'])
 
     def get_success_url(self):
-        return reverse('office:users.detail', kwargs={'pk': self.object.pk})
+        return reverse('office:users.detail', kwargs={'pk': self.kwargs['pk']})
 
 
 # FIXME No implemented yet
