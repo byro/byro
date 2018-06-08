e977506496bc6a25ddfc5b5ca87474697c0ad9de
Tobias Kunze <rixx@cutebit.de>
2018-06-08 16:28:55 +0200

    [doc] Document new nav_event params (95096aa)

--- src/byro/office/signals.py
+++ src/byro/office/signals.py
@@ -17,8 +17,12 @@ Please use byro.office.views.members.MemberView as base class for these views.
 nav_event = django.dispatch.Signal()
 """
 This signal allows you to add additional views to the sidebar.
-Receives the request as sender. Must return a dictionary containing at
-least the keys ``label`` and ``url``. You can also return a ForkAwesome
-icon name iwth the key ``icon``. You should also return an ``active``
-key with a boolean set to ``True`` if this item should be marked as active.
+Receives the request as sender. Must return a dictionary containing at least
+the keys ``label`` and ``url``. You can also return a ForkAwesome icon name
+iwth the key ``icon``. You should also return an ``active`` key with a boolean
+set to ``True`` if this item should be marked as active.
+
+If you want your Plugin to appear in the "Finance" or "Settings" submenu in the
+side bar, please set ``section`` in your return dict to either ``finance`` or
+``settings``, and don't set an ``icon``.
 """
