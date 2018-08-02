196670c6676a2ae25c67f118b05e224b5ab7be41
Tobias Kunze <r@rixx.de>
2018-08-02 23:34:32 +0200

    [common] Make sure queryparams in urls are strings

--- src/byro/common/templatetags/url_replace.py
+++ src/byro/common/templatetags/url_replace.py
@@ -6,5 +6,5 @@ register = template.Library()
 @register.simple_tag
 def url_replace(request, field, value):
     queryparams = request.GET.copy()
-    queryparams[field] = value
+    queryparams[field] = str(value)
     return queryparams.urlencode()
