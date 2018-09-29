0bef2a31466857c8cccbe469f97f5912ebd97905
Tobias Kunze <r@rixx.de>
2018-09-29 11:40:28 +0200

    Remove database defaults from settings

--- src/byro/settings.py
+++ src/byro/settings.py
@@ -124,8 +124,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'byro',
-        'USER': 'byro',
-        'PASSWORD': 'byro',
         'HOST': 'localhost',
     }
 }
