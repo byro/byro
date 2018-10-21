affa7217058e4aa5ed02f5104c3224bfb2f468ee
Henryk Plötz <henryk@ploetzli.ch>
2018-10-22 01:34:20 +0200

    [common] Create and maintain a "log chain", a hash chain of log entries

--- src/byro/common/models/log.py
+++ src/byro/common/models/log.py
@@ -1,15 +1,21 @@
+import base64
 import datetime
 import decimal
 from functools import wraps
 
+import canonicaljson
+import nacl.hash
+import nacl.utils
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.postgres.fields import JSONField
 from django.db import models
+from django.db.models import F, Q
 from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 from django.utils.timezone import now
+
 
 class ContentObjectManager(models.Manager):
     "An object manager that can handle filter(content_object=...)"
@@ -20,11 +26,18 @@ class ContentObjectManager(models.Manager):
             kwargs['object_id'] = content_object.pk
         return super().filter(*args, **kwargs)
 
+
 class LogEntryManager(ContentObjectManager):
     "Manager that is linking the log chain on .create()"
     def create(self, *args, **kwargs):
-        #prev_entry = LogEntry.objects.filter()
+        kwargs['auth_prev'] = self.get_chain_end()
         return super().create(*args, **kwargs)
+
+    def get_chain_end(self):
+        return self.filter(Q(auth_next=None) | Q(auth_prev_id=F('auth_hash'))).first()
+
+
+PERSON_BYTES = b'byro logchain v1'
 
 
 class LogEntry(models.Model):
@@ -46,6 +59,8 @@ class LogEntry(models.Model):
         * hash_ver: hash/algorithm version
         * nonce: A random nonce
         * data_mac: Message Authentication Code for the LogEntry.data attribute.
+        * orig_content_type: Copy of the ContentType value at LogEntry creation
+        * orig_user_id: Copy of the user_id value at LogEntry creation
     """
     content_type = models.ForeignKey(ContentType, null=True, on_delete=models.SET_NULL)
     object_id = models.PositiveIntegerField(db_index=True)
@@ -61,8 +76,6 @@ class LogEntry(models.Model):
     auth_data = JSONField(null=False)
 
     objects = LogEntryManager()
-
-    # FIXME Add pre-save signal, and, possibly, database-level protection against modification
 
     class Meta:
         ordering = ('-datetime', '-id')
@@ -92,7 +105,71 @@ class LogEntry(models.Model):
             if not self.data.get('source'):
                 raise ValueError("Need to provide at least user or data['source']")
 
+            self._compute_logchain()
+
         return super().save(*args, **kwargs)
+
+    def _compute_logchain(self):
+        hashed_data_dict = dict(self.data)
+        hdd_encoded = canonicaljson.encode_canonical_json(hashed_data_dict)
+
+        hdd_nonce = nacl.utils.random(12)
+        hdd_mac = nacl.hash.blake2b(hdd_encoded, digest_size=64, salt=hdd_nonce)
+
+        if not self.datetime:
+            self.datetime = now()
+
+        self.auth_data = {
+            'hash_ver': 1,
+            'nonce': base64.b64encode(hdd_nonce).decode('us-ascii'),
+            'data_mac': 'blake2b:{}'.format(hdd_mac.decode('us-ascii')),
+            'orig_content_type': '{}.{}'.format(self.content_type.app_label, self.content_type.model) if self.content_type else None,
+            'orig_user_id': self.user_id if self.user_id else None,
+        }
+
+        authenticated_dict = self.get_authenticated_dict()
+
+        ad_encoded = canonicaljson.encode_canonical_json(authenticated_dict)
+        self.auth_hash = "blake2b:{}".format(nacl.hash.blake2b(ad_encoded, digest_size=64, person=PERSON_BYTES).decode('us-ascii'))
+
+        if not self.auth_prev_id:
+            self.auth_prev_id = self.auth_hash
+
+    def get_authenticated_dict(self):
+        return {
+            'object_id': self.object_id,
+            'datetime': self.datetime.isoformat(),
+            'action_type': self.action_type,
+            'prev_hash': self.auth_prev_id if self.auth_prev_id and self.auth_prev_id != self.auth_hash else 'initial:0',
+            'auth_data': self.auth_data,
+            'source': self.data['source'],
+        }
+
+    def verify(self):
+        if self.auth_data['hash_ver'] != 1:
+            return False
+
+        if self.content_type is not None and '{}.{}'.format(self.content_type.app_label, self.content_type.model) != self.auth_data['orig_content_type']:
+            return False
+
+        if self.user_id is not None and self.user_id != self.auth_data['orig_user_id']:
+            return False
+
+        hashed_data_dict = dict(self.data)
+        hdd_encoded = canonicaljson.encode_canonical_json(hashed_data_dict)
+
+        hdd_nonce = base64.b64decode(self.auth_data['nonce'])
+        hdd_mac = nacl.hash.blake2b(hdd_encoded, digest_size=64, salt=hdd_nonce)
+
+        if 'blake2b:{}'.format(hdd_mac.decode('us-ascii')) != self.auth_data['data_mac']:  # FIXME Maybe fixed time comparison?
+            return False
+
+        authenticated_dict = self.get_authenticated_dict()
+
+        ad_encoded = canonicaljson.encode_canonical_json(authenticated_dict)
+        auth_hash = "blake2b:{}".format(nacl.hash.blake2b(ad_encoded, digest_size=64, person=PERSON_BYTES).decode('us-ascii'))
+
+        return auth_hash == self.auth_hash  # FIXME Maybe fixed time comparison?
 
 
 @receiver(pre_save, sender=LogEntry)
