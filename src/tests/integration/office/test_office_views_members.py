1df6fe9d75e5c3a42772392bf1c39cdc1dc2ed53
Henryk Plötz <henryk@ploetzli.ch>
2018-11-04 16:44:48 +0100

    [tests] Improve test coverage

--- src/tests/integration/office/test_office_views_members.py
+++ src/tests/integration/office/test_office_views_members.py
@@ -1,4 +1,6 @@
 import pytest
+from dateutil.relativedelta import relativedelta
+from django.utils.timezone import now
 from django.urls import reverse
 
 pytestmark = pytest.mark.usefixtures('configuration')
@@ -40,3 +42,65 @@ def test_all_members_list(member, membership, inactive_member, logged_in_client)
     assert response.status_code == 200, content
     assert member.name in content
     assert inactive_member.name in content
+
+
+@pytest.mark.django_db
+def test_members_export_list_csv(member, membership, inactive_member, logged_in_client):
+    response = logged_in_client.post(
+        reverse('office:members.list.export'), {
+            'member_filter': 'all',
+            'export_format': 'csv',
+            'field_list': ['_internal_id', 'member__name'],
+        }
+    )
+    content = b"".join(response.streaming_content).decode()
+    assert response.status_code == 200, content
+    assert content == "\ufeffInternal database ID,Name\r\n{},{}\r\n{},{}\r\n".format(inactive_member.pk, inactive_member.name, member.pk, member.name)
+
+
+@pytest.mark.django_db
+def test_members_adjust_account_initial(member, logged_in_client):
+    assert member.balance == 0
+    response = logged_in_client.post(
+        reverse('office:members.operations', kwargs={'pk': member.pk}), {
+            'member_account_adjustment-date': str(now().date()),
+            'member_account_adjustment-adjustment_reason': 'initial',
+            'member_account_adjustment-adjustment_type': 'absolute',
+            'member_account_adjustment-amount': '23',
+            'submit_member_account_adjustment_adjust': 'adjust',
+        }
+    )
+    content = response.content.decode()
+    assert response.status_code == 302, content
+    assert member.balance == 23
+
+
+@pytest.mark.django_db
+def test_members_adjust_account_waiver(member, logged_in_client):
+    assert member.balance == 0
+    response = logged_in_client.post(
+        reverse('office:members.operations', kwargs={'pk': member.pk}), {
+            'member_account_adjustment-date': str(now().date()),
+            'member_account_adjustment-adjustment_reason': 'waiver',
+            'member_account_adjustment-adjustment_type': 'relative',
+            'member_account_adjustment-amount': '-2',
+            'submit_member_account_adjustment_adjust': 'adjust',
+        }
+    )
+    content = response.content.decode()
+    assert response.status_code == 302, content
+    assert member.balance == 2
+
+
+@pytest.mark.django_db
+def test_members_end_membership(member, membership, logged_in_client):
+    assert member.is_active
+    response = logged_in_client.post(
+        reverse('office:members.operations', kwargs={'pk': member.pk}), {
+            'ms_{}_leave-end'.format(membership.pk): (now() + relativedelta(days=-1)).date(),
+            'submit_ms_{}_leave_end'.format(membership.pk): 'end',
+        }
+    )
+    content = response.content.decode()
+    assert response.status_code == 302, content
+    assert not member.is_active
