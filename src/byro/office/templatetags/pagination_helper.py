faf4ee85cedfdd18a6824b1bbe6cc0c3724d1036
Henryk Plötz <henryk@ploetzli.ch>
2019-01-28 14:44:53 +0100

    [office] Improve pagination

---
+++ src/byro/office/templatetags/pagination_helper.py
@@ -0,0 +1,16 @@
+from django.template.defaultfilters import register
+
+
+@register.filter(name='paginate_loop')
+def translate_document_category(page_obj, context=5):
+    items = [1, page_obj.number, page_obj.paginator.num_pages]
+    for i in range(context):
+        items.append(max(1, page_obj.number - i))
+        items.append(min(page_obj.number + i, page_obj.paginator.num_pages))
+
+    last = 0
+    for item in sorted(set(items)):
+        if not last + 1 == item:
+            yield None
+        yield item
+        last = item
