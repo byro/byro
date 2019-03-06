d0fef80711eca53aa00e76e7ddc184f1f1bf43a2
Tobias Kunze <r@rixx.de>
2019-03-07 00:11:38 +0100

    Add separate test settings

---
+++ src/byro/common/settings/test_settings.py
@@ -0,0 +1,48 @@
+import atexit
+import os
+import tempfile
+from contextlib import suppress
+
+tmpdir = tempfile.TemporaryDirectory()
+os.environ.setdefault('DATA_DIR', tmpdir.name)
+
+from byro.settings import *  # noqa
+
+BASE_DIR = tmpdir.name
+DATA_DIR = tmpdir.name
+LOG_DIR = os.path.join(DATA_DIR, 'logs')
+MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
+STATIC_ROOT = os.path.join(DATA_DIR, 'static')
+HTMLEXPORT_ROOT = os.path.join(DATA_DIR, 'htmlexport')
+
+for directory in (BASE_DIR, DATA_DIR, LOG_DIR, MEDIA_ROOT, HTMLEXPORT_ROOT):
+    os.makedirs(directory, exist_ok=True)
+
+atexit.register(tmpdir.cleanup)
+
+EMAIL_BACKEND = 'django.core.mail.outbox'
+MAIL_FROM = 'orga@orga.org'
+
+COMPRESS_ENABLED = COMPRESS_OFFLINE = False
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+DEBUG = True
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+with suppress(ValueError):
+    INSTALLED_APPS.remove('debug_toolbar.apps.DebugToolbarConfig')  # noqa
+    MIDDLEWARE.remove('debug_toolbar.middleware.DebugToolbarMiddleware')  # noqa
+
+
+# Don't run migrations
+class DisableMigrations(object):
+
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return None
+
+
+if not os.environ.get("TRAVIS", ""):
+    MIGRATION_MODULES = DisableMigrations()
