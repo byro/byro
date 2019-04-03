a8a0dac8280ebf07912592e9cc6d81a02c2253da
Tobias Kunze <r@rixx.de>
2019-04-03 11:44:38 +0200

    [travis]

--- .travis.sh
+++ .travis.sh
@@ -5,21 +5,19 @@ echo "Running $1"
 
 if [ "$1" == "tests" ]; then
     psql -c 'create database byro;' -U postgres
-    cd src
     python -m byro check
     pytest --cov=byro tests
     codecov
 fi
 
 if [ "$1" == "style" ]; then
-    cd src
     pylama .
     isort --check-only --recursive --diff .
 fi
 
 if [ "$1" == "docs" ]; then
     pip install -r src/requirements/documentation.txt
-    cd docs
+    cd ../docs
     make html
     make linkcheck
     npm install -g write-good
