9ecbf3f05fa41f6dabed4726ba35dc7890b9bce8
Annika Hannig <annika@hannig.cc>
2020-07-04 21:26:31 +0200

    Fix missing attribute  on real transactions source

--- src/byro/bookkeeping/models/real_transaction.py
+++ src/byro/bookkeeping/models/real_transaction.py
@@ -64,3 +64,23 @@ class RealTransactionSource(Auditable, models.Model, LogTargetMixin):
         self.state = SourceState.PROCESSED
         self.save()
         return response
+
+    @property
+    def transactions(self):
+        """Get all transactions"""
+        return TransactionSet(self)
+
+
+class TransactionSet:
+    """
+    This is a pseudo related manager, providing a minimal
+    implementation for `all()`.
+    """
+    def __init__(self, source):
+        """Set real transaction source"""
+        self.source = source
+
+    def all(self):
+        """Get all transactions via bookings"""
+        return (b.transaction for b in self.source.bookings.all())
+
