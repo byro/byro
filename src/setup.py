5300b7ea10ed30bc8b90f650aa03b38027614a95
Tobias Kunze <r@rixx.de>
2020-08-19 11:23:32 +0200

    Update more dependencies

--- src/setup.py
+++ src/setup.py
@@ -65,31 +65,31 @@ setup(
     ],
     keywords="members membership fees club group clubs associations association",
     install_requires=[
-        "canonicaljson==1.1.4",  # https://github.com/matrix-org/python-canonicaljson/blob/master/CHANGES.md
-        "chardet~=3.0",  # https://github.com/chardet/chardet/releases
-        "celery~=4.3",  # search for "what's new" on http://docs.celeryproject.org/en/latest/
-        "csscompressor~=0.9",  # 2017-11, no changelog, https://github.com/sprymix/csscompressor
-        "dateparser~=0.7",  # https://github.com/scrapinghub/dateparser/blob/master/HISTORY.rst
-        "Django~=3.1",  # https://docs.djangoproject.com/en/2.0/releases/
-        "django-annoying~=0.10",  # https://github.com/skorokithakis/django-annoying/releases
-        "django-bootstrap4~=2.0.0",  # http://django-bootstrap4.readthedocs.io/en/latest/history.html
-        "django-compressor~=2.4",  # https://django-compressor.readthedocs.io/en/latest/changelog/
-        "django-extensions~=2.2",  # https://github.com/django-extensions/django-extensions/blob/master/CHANGELOG.md
+        "canonicaljson==1.3.0",  # https://github.com/matrix-org/python-canonicaljson/blob/master/CHANGES.md
+        "chardet~=3.0.0",  # https://github.com/chardet/chardet/releases
+        "celery~=4.3.0",  # search for "what's new" on http://docs.celeryproject.org/en/latest/
+        "csscompressor~=0.9.0",  # 2017-11, no changelog, https://github.com/sprymix/csscompressor
+        "dateparser~=0.7.0",  # https://github.com/scrapinghub/dateparser/blob/master/HISTORY.rst
+        "Django~=3.1.0",  # https://docs.djangoproject.com/en/2.0/releases/
+        "django-annoying~=0.10.0",  # https://github.com/skorokithakis/django-annoying/releases
+        "django-bootstrap4~=2.2.0",  # http://django-bootstrap4.readthedocs.io/en/latest/history.html
+        "django-compressor~=2.4.0",  # https://django-compressor.readthedocs.io/en/latest/changelog/
+        "django-extensions~=3.0.0",  # https://github.com/django-extensions/django-extensions/blob/master/CHANGELOG.md
         "django-formset-js-improved==0.5.0.2",  # no changelog, https://github.com/pretix/django-formset-js
-        "django-i18nfield~=1.7",  # 2017-11, no changelog, https://github.com/raphaelm/django-i18nfield/
-        "django-libsass~=0.8",  # inactive, https://github.com/torchbox/django-libsass/blob/master/CHANGELOG.txt
-        "django-localflavor~=3.0",
-        "django-select2~=7.4",  # https://github.com/applegrew/django-select2/releases
-        "django-solo~=1.1",  # https://github.com/lazybird/django-solo/blob/master/CHANGES
+        "django-i18nfield~=1.7.0",  # 2017-11, no changelog, https://github.com/raphaelm/django-i18nfield/
+        "django-libsass~=0.8.0",  # inactive, https://github.com/torchbox/django-libsass/blob/master/CHANGELOG.txt
+        "django-localflavor~=3.0.0",
+        "django-select2~=7.4.0",  # https://github.com/applegrew/django-select2/releases
+        "django-solo~=1.1.0",  # https://github.com/lazybird/django-solo/blob/master/CHANGES
         "inlinestyler~=0.2",  # https://github.com/dlanger/inlinestyler/blob/master/CHANGELOG
         "jinja2>=2.10.1",  # https://github.com/pallets/jinja/blob/master/CHANGES.rst
         "psycopg2-binary",
         "python-dateutil",
-        "python-magic==0.4.15",
-        "pynacl~=1.4",  # https://github.com/pyca/pynacl/blob/master/CHANGELOG.rst
+        "python-magic~=0.4.0",
+        "pynacl~=1.4.0",  # https://github.com/pyca/pynacl/blob/master/CHANGELOG.rst
         "qrcode[pil]==6.1",  # https://github.com/lincolnloop/python-qrcode/blob/master/CHANGES.rst
-        "unicodecsv~=0.14",
-        "more-itertools~=7.0",
+        "unicodecsv~=0.14.0",
+        "more-itertools~=8.4.0",
         "schwifty==2020.8",
     ],
     extras_require={
