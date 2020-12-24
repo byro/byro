7ba0ed562068ab7c896d07cb074112b855a989ba
Henryk Plötz <henryk@ploetzli.ch>
2020-12-24 11:54:01 +0100

    Run byro as normal user

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -3,10 +3,11 @@ set -e
 
 
 if [ "$1" = 'byro' ]; then
-  su -p uid1000 -c "django-admin migrate"
-  exec gunicorn --bind '[::]:80' --max-requests 1200 --max-requests-jitter 50 --log-level=info --worker-tmp-dir /dev/shm --workers "${GUNICORN_WORKERS:-3}" --user uid1000 --group uid1000 byro.wsgi
+  su -p "${BYRO_USER}" -c "django-admin migrate"
+  chown -R "${BYRO_USER}:${BYRO_USER}" /byro/data/*/*
+  exec gunicorn --bind '[::]:80' --max-requests 1200 --max-requests-jitter 50 --log-level=info --worker-tmp-dir /dev/shm --workers "${GUNICORN_WORKERS:-3}" --user "${BYRO_USER}" --group "${BYRO_USER}" byro.wsgi
 elif [ "$1" = 'django-admin' ]; then
-  exec su -p uid1000 -c 'django-admin "$@"' -- "$@"
+  exec su -p "${BYRO_USER}" -c 'django-admin "$@"' -- "$@"
 fi
 
 exec "$@"
