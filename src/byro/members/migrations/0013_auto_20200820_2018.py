6805f040497f77ce084bf92e2c26d2419d6af8a9
Nis Wechselberg <enbewe@enbewe.de>
2021-10-16 17:19:30 +0200

    format: Adjusted code formatting through black
    Some files did not conform to coding guidelines.
    
    Signed-off-by: Nis Wechselberg <enbewe@enbewe.de>

--- src/byro/members/migrations/0013_auto_20200820_2018.py
+++ src/byro/members/migrations/0013_auto_20200820_2018.py
@@ -11,6 +11,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AlterModelOptions(
-            name="member", options={"ordering": (("direct_address_name", "name"),)},
+            name="member",
+            options={"ordering": (("direct_address_name", "name"),)},
         ),
     ]
