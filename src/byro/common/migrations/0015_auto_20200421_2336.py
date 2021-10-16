68c7faf885611f8e1314dd3e89a0abffd6c71e94
Nis Wechselberg <enbewe@enbewe.de>
2021-10-16 17:19:30 +0200

    format: Adjusted code formatting through black
    Some files did not conform to coding guidelines.
    
    Signed-off-by: Nis Wechselberg <enbewe@enbewe.de>

--- src/byro/common/migrations/0015_auto_20200421_2336.py
+++ src/byro/common/migrations/0015_auto_20200421_2336.py
@@ -11,7 +11,8 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RemoveField(
-            model_name="configuration", name="can_see_other_members",
+            model_name="configuration",
+            name="can_see_other_members",
         ),
         migrations.AlterField(
             model_name="configuration",
