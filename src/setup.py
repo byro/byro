9e6160a7c4d5b33bf1edabc2eaeb231ff2fb7f06
Tobias Kunze <r@rixx.de>
2021-10-29 11:34:14 +0200

    Update Django

--- src/setup.py
+++ src/setup.py
@@ -71,10 +71,10 @@ setup(
         "celery>=5.1,<6.0",  # search for "what's new" on http://docs.celeryproject.org/en/latest/
         "csscompressor~=0.9.5",  # 2017-11, no changelog, https://github.com/sprymix/csscompressor
         "dateparser>=1.1,<1.2",  # https://github.com/scrapinghub/dateparser/blob/master/HISTORY.rst
-        "Django>=3.1,<3.2",  # https://docs.djangoproject.com/en/2.0/releases/
+        "Django>=3.2,<3.3",  # https://docs.djangoproject.com/en/2.0/releases/
         "django-annoying~=0.10.0",  # https://github.com/skorokithakis/django-annoying/releases
         "django-bootstrap4>=3.0,<3.1",  # http://django-bootstrap4.readthedocs.io/en/latest/history.html
-        "django-compressor~=2.4.0",  # https://django-compressor.readthedocs.io/en/latest/changelog/
+        "django-compressor~=2.4",  # https://django-compressor.readthedocs.io/en/latest/changelog/
         "django-extensions>=3.1,<3.2",  # https://github.com/django-extensions/django-extensions/blob/master/CHANGELOG.md
         "django-formset-js-improved==0.5.0.2",  # no changelog, https://github.com/pretix/django-formset-js
         "django-i18nfield>=1.9,<1.10",  # 2017-11, no changelog, https://github.com/raphaelm/django-i18nfield/
