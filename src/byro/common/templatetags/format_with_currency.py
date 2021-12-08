bd4af81fe607efe5b2f7f8942965a6de2c40be35
András Veres-Szentkirályi <vsza@vsza.hu>
2021-12-08 21:59:52 +0100

    made monetary formatting more configurable

---
+++ src/byro/common/templatetags/format_with_currency.py
@@ -0,0 +1,13 @@
+from django import template
+
+from byro.common.models import Configuration
+
+register = template.Library()
+
+
+@register.filter
+def format_with_currency(value, long_form=False):
+    config = Configuration.get_solo()
+    amount = str(value if config.display_cents else int(value))
+    parts = [amount, config.currency if long_form else config.currency_symbol]
+    return ' '.join(parts if config.currency_postfix else reversed(parts))
