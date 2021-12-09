41821bb647c145eadaec0bb6364322a538e2b793
András Veres-Szentkirályi <vsza@vsza.hu>
2021-12-09 09:25:38 +0100

    reformatted changes related to #223 with black

--- src/byro/common/templatetags/format_with_currency.py
+++ src/byro/common/templatetags/format_with_currency.py
@@ -10,4 +10,4 @@ def format_with_currency(value, long_form=False):
     config = Configuration.get_solo()
     amount = str(value if config.display_cents else int(value))
     parts = [amount, config.currency if long_form else config.currency_symbol]
-    return ' '.join(parts if config.currency_postfix else reversed(parts))
+    return " ".join(parts if config.currency_postfix else reversed(parts))
