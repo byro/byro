d5361656f74da4c4f20226816f7ec8f1cf0c07cf
András Veres-Szentkirályi <vsza@vsza.hu>
2021-12-09 09:52:40 +0100

    handle config.currency = NULL gracefully

--- src/byro/common/templatetags/format_with_currency.py
+++ src/byro/common/templatetags/format_with_currency.py
@@ -9,5 +9,9 @@ register = template.Library()
 def format_with_currency(value, long_form=False):
     config = Configuration.get_solo()
     amount = str(value if config.display_cents else int(value))
-    parts = [amount, config.currency if long_form else config.currency_symbol]
-    return " ".join(parts if config.currency_postfix else reversed(parts))
+    currency = config.currency if long_form else config.currency_symbol
+    if currency:
+        parts = [amount, currency]
+        return " ".join(parts if config.currency_postfix else reversed(parts))
+    else:
+        return amount
