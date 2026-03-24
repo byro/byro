b401d7c4c798f35116f5464142956ff7bfeb6b31
Luca Schröder <git@sndstrm.de>
2026-03-24 17:47:39 +0100

    Make all API endpoints public

--- src/byro/api/signals.py
+++ src/byro/api/signals.py
@@ -3,6 +3,11 @@ from django.dispatch import receiver
 from byro.common.signals import unauthenticated_urls
 
 
+def _is_api_request(request, url):
+    return url.namespace == "api"
+
+
 @receiver(unauthenticated_urls)
-def api_docs_unauthenticated_urls(sender, **kwargs):
-    return ("api:schema", "api:swagger-ui")
+def api_unauthenticated_urls(sender, **kwargs):
+    return [_is_api_request]
+
